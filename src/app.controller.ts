ccc6095ab591dcf30f041ab2d90cefef9fed448a
RohanJohnson <21000165@students.gateshead.ac.uk>
2022-12-14 14:38:35 +0000

    API init plus a few services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { myList } from './app.service';
+
+export class NewItemInput{
+  text: string
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('/')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/list')
+  getList(): any[] {
+    return this.appService.getList();
+  }
+
+  @Post('/add')
+  addToList(@Body() body: NewItemInput): any[] {
+    const { text } = body;
+    return this.appService.addToList(text);
+  }
+
+
+  @Get('/mov')
+  getMovie(): Promise<any[]>{
+    return this.appService.getMovie();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
+
+export let myList = ["beanz", "ketchup", "spaghetti hoops"];
+
+import axios from 'axios';
+
+const config = {
+  method: 'get',
+  url: 'https://api.themoviedb.org/3/trending/movie/week?api_key=c14286efd49ae138e2d5e2661df06122',
+  headers: {}
+};
+
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getList(): any[] {
+    return myList;
+  }
+
+  addToList(newItem): any[] {
+    myList.push(newItem);
+    return myList;
+  }
+
+  async getMovie(): Promise<any[]> {
+    const response = await axios(config);
+    const filtered = response.data.results.map(movie => movie.title);
+    return filtered;
+
+  }
 }
+
